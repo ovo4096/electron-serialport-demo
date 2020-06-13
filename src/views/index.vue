1e0da0a2de5092154ffb1226b8d29a99930437fd
Eric Xue <ovo4096@gmail.com>
2020-06-13 20:26:41 +0800

    demo

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,23 +1,20 @@
 <template>
   <div>
-    <button @click="cmd1">指令1</button>
-    <button @click="cmd2">指令2</button>
+    <button @click="cmd1">亮</button>
+    <button @click="cmd2">灭</button>
   </div>
 </template>
 
 <script>
+import { ipcRenderer } from 'electron';
+
 export default {
-  data() {
-    return {
-      port: null,
-    };
-  },
-  mounted() {
-  },
   methods: {
     cmd1() {
+      ipcRenderer.send('serialPort:write', Buffer.from([0x01, 0x05, 0x00, 0x00, 0xFF, 0x00, 0x8C, 0x3A]));
     },
     cmd2() {
+      ipcRenderer.send('serialPort:write', Buffer.from([0x01, 0x05, 0x00, 0x00, 0x00, 0x00, 0xCD, 0xCA]));
     },
   },
 };
